75417786fa1f07ae556ca5047e6a01faf47260ef
Scott <tsgcats@gmail.com>
2025-03-03 07:54:14 -0500

    Made HTML fields

--- js/javasript.js
+++ js/javasript.js
@@ -1,5 +1,51 @@
+console.table({first_name: 'Scott', email: 'Me@me', 
+               last_name:'George', phone: '202-222-5555'})
+//adding styles
+const styles = 'padding: 40px; background-color: yellow; color: green;'
 
-let myJsPractice = "pracice";
+console.log('%cHello', styles)
 
-console.log(myJsPractice);
+//Arithmetic operators
+let x;
 
+x = 5;
+x = true;
+x = 100 % 9;
+
+//exponent
+x = 3 ** 2;
+x = 1;
+
+//increment 
+x++;
+
+//decrement
+x--;
+
+//Assignment operators
+
+x = 10;
+
+x *= 10;
+
+x -= 4;
+
+x %= 95;
+
+// Comparison Operators
+
+// double == is Testing value
+x = 6 == '6';
+//tripple === is Testing value and type
+x = 6 === '6';
+
+// not equal to .. tesing value
+x = 3 != 4;
+
+// not equal to .. tesing value and type 
+x = 3 !== 's';
+
+
+
+
+console.log(x);
